fdd668cb70bb39950566f8676c4c17eb1409161f
Pavel Minkov <pavel.minkov.it@gmail.com>
2024-04-14 22:18:46 +0300

    Add project files.

---
+++ LoginTest/LoginAutomationTest.cs
@@ -0,0 +1,45 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace LoginTest
+{
+    [TestFixture]
+    public class LoginAutomationTest
+    {
+
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            var chromeDriverPath = "path of driver";
+            driver = new ChromeDriver(chromeDriverPath);
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://qa.sorted.com/nextrack");
+        }
+
+        [Test]
+        public void LoginTest()
+        {
+
+            driver.FindElement(By.Id("email")).SendKeys("johnsmith@sorted.com");
+            driver.FindElement(By.Id("password")).SendKeys("Pa55w0rd!");
+            driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(driver => driver.Url.Contains("loginsuccess"));
+
+            var actualUrl = "http://qa.sorted.com/nextrack/loginsuccess";
+            var expectedURL = driver.Url;
+            Assert.That(expectedURL, Is.EqualTo(actualUrl));
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+    }
+}
